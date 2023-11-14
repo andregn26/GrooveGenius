66bdc212df677df7c394a4260ea3891a7b249700
andregn26 <andregn26@gmail.com>
2023-11-14 18:33:38 +0000

    get access token done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchResults from "./components/SearchResults";
 import Playlist from "./components/Playlist";
 import SearchBar from "./components/SearchBar";
@@ -7,10 +7,38 @@ import Tracklist from "./components/Tracklist";
 import { getSongs } from "./spotify";
 import "./App.css";
 
+const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET;
+
 function App() {
+	const [accessToken, setAccessToken] = useState("");
 	const [spotifySongs, setSpotifySongs] = useState([]);
 	const [chosenSongs, setChosenSongs] = useState([]);
 	const [myPlaylistTitle, setMyPlaylistTitle] = useState("Teste titulo playlist");
+
+	// API Access Token
+	useEffect(() => {
+		fetch("https://accounts.spotify.com/api/token", {
+			method: "POST",
+			headers: { "Content-Type": "application/x-www-form-urlencoded" },
+			body: `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`,
+		})
+			.then((response) => response.json())
+			.then((data) => setAccessToken(data.access_token));
+	}, []);
+
+	// Get Artist
+	const getArtist = () => {
+		fetch("https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb", {
+			headers: {
+				Authorization: `Bearer  ${accessToken}`,
+			},
+		})
+			.then((response) => response.json())
+			.then((data) => console.log(data));
+	};
+
+	const getSearch = () => {};
 
 	const handleSearch = async (query) => {
 		const searchResult = await getSongs(query);
@@ -28,14 +56,6 @@ function App() {
 		setChosenSongs((myTracks) => myTracks.filter((track) => track.id !== trackIdToRemove));
 	};
 
-	const savePlaylist = () => {
-		let songsURI = [];
-		chosenSongs.forEach((track) => {
-			songsURI.push(track.uri);
-		});
-		return songsURI;
-	};
-
 	return (
 		<div className="scrollbar bg-slate-50 text-slate-900 min-h-screen">
 			<header className="flex  justify-center">
@@ -45,6 +65,7 @@ function App() {
 				<SearchResults trackList={spotifySongs} addTrack={addTrack} isSearchResults={true} />
 				<Playlist trackList={chosenSongs} removeTrack={removeTrack} isSearchResults={false} />
 			</main>
+			<button onClick={getArtist}>Artist</button>
 		</div>
 	);
 }
